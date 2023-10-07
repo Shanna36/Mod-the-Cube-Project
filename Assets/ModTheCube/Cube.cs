bf6451998e6d2670f5bf9b2821f99870687ea3a4
Shelby Hanna <144790272+Shanna36@users.noreply.github.com>
2023-10-07 21:07:47 +0100

    Colour changing over time
    added more colours and auto transition over time

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -5,22 +5,49 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    public Color cubeColor = new Color (0.5f, 1.0f, 0.3f, 0.4f);
-    public Vector3 position = new Vector3(3, 4, 1);
-
     
+    public float duration = 5.0f; // Time taken for each color transition
+
+    private int colorIndex = 0;
+    private float startTime;
+
+    public Color[] colors = new Color[]
+{
+    new Color(1.0f, 0.0f, 0.0f, 1.0f), // Red
+    new Color(0.0f, 1.0f, 0.0f, 1.0f), // Green
+    new Color(0.0f, 0.0f, 1.0f, 1.0f), // Blue
+    new Color(1.0f, 1.0f, 0.0f, 1.0f), // Yellow
+    // Add more colors as needed
+};
+
+
     void Start()
     {
-        transform.position = (position); 
+        transform.position = new Vector3(3, 4, 1);
         transform.localScale = Vector3.one * 1.3f;
-        
+
         Material material = Renderer.material;
-        
-        material.color = cubeColor;
+
+        material.color = colors[0];
+        startTime = Time.time;
     }
-    
+
     void Update()
     {
+        // Calculate the lerp value based on time
+        float t = (Time.time - startTime) / duration;
+        // Set the color using Lerp
+        Renderer.material.color = Color.Lerp(colors[colorIndex], colors[(colorIndex + 1) % colors.Length], t);
+
+        if (t >= 1.0f)
+        {
+            // Reset the start time for the next color transition
+            startTime = Time.time;
+            // Move to the next color in the array
+            colorIndex = (colorIndex + 1) % colors.Length;
+        }
+
         transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
     }
 }
+
